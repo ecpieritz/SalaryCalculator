0f67c61325ae813bd6679b16190e0a997d9b9f99
Emilyn Pieritz <66131506+EPieritz@users.noreply.github.com>
2020-08-12 11:21:54 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import 'materialize-css/dist/css/materialize.min.css';
+import './css/index.css';
+
+ReactDOM.render(<React.StrictMode><App /></React.StrictMode>, document.getElementById('root'));
